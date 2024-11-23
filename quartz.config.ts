3562a7a145a9b5bc6dd58f6176930e18521a9613
29A16_Hall <johannesroylands@protonmail.com>
2024-11-23 10:54:26 +0100

    Quartz sync: Nov 23, 2024, 10:54 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴Note Collection 29a16",
+    pageTitle: "🌱29a16",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
